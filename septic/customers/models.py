5b6305482505c3887cb7de1e72dff3392864c816
codymart1986 <codymart1986@hotmail.com>
2021-09-13 07:57:28 -0700

    created model for customer

--- septic/customers/models.py
+++ septic/customers/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey('accounts.user', blank=True, null=True, on_delete=models.PROTECT)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    zip_code = models.IntegerField(default=00000, null=True)
+    balance = models.IntegerField(default=0)
