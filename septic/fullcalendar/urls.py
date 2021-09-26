c6cea6394aeeb024f832ea61171b1659edb271f3
codymart1986 <codymart1986@hotmail.com>
2021-09-26 12:21:59 -0700

    updating calendar

--- septic/fullcalendar/urls.py
+++ septic/fullcalendar/urls.py
@@ -1,11 +1,12 @@
 from .views import calendar, add_event, update, remove, all_events
 from django.urls import path, include
+from django.conf.urls import include, url
 
 app_name = "fullcalendar"
 urlpatterns = [
-path('calendar', calendar, name='calendar'),
-path('add_event', add_event, name='add_event'),
-path('update', update, name='update'),
-path('remove', remove, name='remove'),
-path('all_events', all_events, name='all_events')
+url('^calendar', calendar, name='calendar'),
+url('^add_event$', add_event, name='add_event'),
+url('^update$', update, name='update'),
+url('^remove', remove, name='remove'),
+url('^all_events', all_events, name='all_events')
 ]
